a6d59828913ceb8007589e26af1c9dda26b9555e
mattcoates <mc955@cam.ac.uk>
2017-02-20 15:03:55 +0000

    Added link timing

---
+++ week_one_tests/link_timing_test.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <robot_instr.h>
+#include <robot_link.h>
+#include <stopwatch.h>
+using namespace std;
+
+#define ROBOT_NUM  9
+robot_link rlink;                           
+stopwatch watch;
+
+/* Entry Point */
+int main () {
+
+    /* Setup Link */
+    int val;                          
+    if (!rlink.initialise (ROBOT_NUM)) {
+    cout << "Cannot initialise link" << endl;
+    rlink.print_errs("    ");
+    return -1;
+    }
+    
+    /* Test response time */
+    watch.start();
+    for(int i=0; i<100; i++){
+        val = rlink.request (TEST_INSTRUCTION);   // send test instruction
+        if (val == REQUEST_ERROR) {
+            cout << "Fatal Error: " << endl;
+            rlink.print_errs();
+            return -1;
+       }
+    }
+    cout << "Time for 1 transmission: " << (watch.stop()/100) << "ms" << endl;
+    
+    return 0;
+}
