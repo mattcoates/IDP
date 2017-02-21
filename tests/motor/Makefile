f3f22694cb3fe24c45fbf11a82cff194a277ef64
mattcoates <mc955@cam.ac.uk>
2017-02-21 10:07:57 +0000

    added motor

---
+++ tests/motor/Makefile
@@ -0,0 +1,3 @@
+# Automatically generated Makefile
+motor : motor.cc 
+	 g++ -ansi -Wall -g -I ../../artifacts/1BRobot -L ../../artifacts/1BRobot motor.cc -o motor_test -lrobot
---
+++ tests/motor/motor.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <robot_instr.h>
+#include <robot_link.h>
+#include <stopwatch.h>
+#include <robot_delay.h>
+using namespace std;
+
+#define ROBOT_NUM  15
+robot_link rlink;                           
+stopwatch watch;
+
+/* Entry Point */
+int main () {
+
+    /* Setup Link */                        
+    if (!rlink.initialise (ROBOT_NUM)) {
+        cout << "Cannot initialise link" << endl;
+        rlink.print_errs("    ");
+        return -1;
+    }
+    
+    
+    rlink.command(MOTOR_1_GO, 50);
+    rlink.command(MOTOR_2_GO, 50);
+    rlink.command(MOTOR_3_GO, 50);
+
+    delay(5000);
+    
+    return 0;
+}
