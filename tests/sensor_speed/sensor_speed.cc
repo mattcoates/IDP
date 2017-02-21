4cad134786bc8ad4dbe0ef5a16374e24fe54cc47
E.J. Taylor <ejt61@ew126.eng.cam.ac.uk>
2017-02-21 09:48:44 +0000

    Fuck you

--- tests/sensor_speed/sensor_speed.cc
+++ tests/sensor_speed/sensor_speed.cc
@@ -15,7 +15,11 @@ int main () {
     cout << "Sensor Testing" << endl;
     
     /* Setup Link */
-    if (!rlink.initialise (ROBOT_NUM)) {
+    #ifdef __arm__
+       if (!rlink.initialise ("127.0.0.1")) {          // setup for local hardware
+    #else
+       if (!rlink.initialise (ROBOT_NUM)) { // setup the link
+    #endif
         cout << "Cannot initialise link" << endl;
         rlink.print_errs("    ");
         return -1;
@@ -24,7 +28,7 @@ int main () {
     /* Test Ping Time */
     int val = 0;
     watch.start();
-    for(int i=0; i<1000; i++) {
+    for(int i=0; i<10000; i++) {
         val = rlink.request (TEST_INSTRUCTION);
         if (val == REQUEST_ERROR) {
             cout << "Fatal Error: " << endl;
@@ -32,30 +36,30 @@ int main () {
             return -1;
        }
     }
-    double ping_time = (watch.stop()/1000);
+    double ping_time = (watch.stop()/10000);
     
     /* Write Byte Time */
     watch.start();
-    for(int j=0; j<1000; j++) {
+    for(int j=0; j<10000; j++) {
        rlink.command(WRITE_PORT_3, 0xAA);     
     }
-    double write_time = (watch.stop()/1000);
+    double write_time = (watch.stop()/10000);
     
     /* Read Byte Time */
     int v = 0;
     watch.start();
-    for(int k=0; k<1000; k++) {
+    for(int k=0; k<10000; k++) {
        v = rlink.request (READ_PORT_3);    
     }
-    double read_time = (watch.stop()/1000);
+    double read_time = (watch.stop()/10000);
     
     /* Read ADC Time */
     int a = 0;
     watch.start();
-    for(int l=0; l<1000; l++) {
+    for(int l=0; l<10000; l++) {
        a = rlink.request (ADC3);   
     }
-    double read_adc_time = (watch.stop()/1000);
+    double read_adc_time = (watch.stop()/10000);
     
     
     /* Output Results */   
